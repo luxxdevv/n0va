2c82ee1e9be3dffaba027624ae6851a0908756d1
Minty <al.vel.guz@gmail.com>
2025-01-11 14:52:02 -0400

    Update MetadataProvider.tsx

--- app/MetadataProvider.tsx
+++ app/MetadataProvider.tsx
@@ -36,71 +36,101 @@ const defaultMetadata = {
 }
 
 const specificMetadata = {
-  title: 'n0va.one - Owner | Luxx.',
-  description: 'Luxx, The owner of the website',
-  openGraph: {
+  '/1': {
     title: 'n0va.one - Owner | Luxx.',
     description: 'Luxx, The owner of the website',
-    url: 'https://n0va.one/1',
-    siteName: 'n0va.one',
-    images: [
-      {
-        url: 'https://n0va.one/profile-picture.png',
-        width: 1000,
-        height: 1000,
-        alt: 'PFP',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
+    openGraph: {
+      title: 'n0va.one - Owner | Luxx.',
+      description: 'Luxx, The owner of the website',
+      url: 'https://n0va.one/1',
+      siteName: 'n0va.one',
+      images: [
+        {
+          url: 'https://n0va.one/profile-picture.png',
+          width: 1000,
+          height: 1000,
+          alt: 'PFP',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'n0va.one - Owner | Luxx.',
+      description: 'Luxx, The owner of the website',
+      creator: '@luxxified',
+      images: ['https://n0va.one/profile-picture.png'],
+    },
+    authors: [{ name: 'luxxified.' }],
+    creator: 'luxxified.',
+    publisher: 'luxxified.',
   },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'n0va.one - Owner | Luxx.',
-    description: 'Luxx, The owner of the website',
-    creator: '@luxxified',
-    images: ['https://n0va.one/profile-picture.png'],
+  '/foreverhiro': {
+    title: 'n0va.one - User | Hiro',
+    description: 'Student Athlete, Class of 29, Gamer.',
+    openGraph: {
+      title: 'n0va.one - User | Hiro',
+      description: 'Student Athlete, Class of 29, Gamer',
+      url: 'https://n0va.one/foreverhiro',
+      siteName: 'n0va.one',
+      images: [
+        {
+          url: 'https://n0va.one/profile-picture.jpg',
+          width: 800,
+          height: 800,
+          alt: 'Hiro Preview',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'n0va.one - User | Hiro',
+      description: 'Student Athlete, Class of 29, Gamer',
+      creator: '@foreverhiro',
+      images: ['https://n0va.one/profile-picture.jpg'],
+    },
+    authors: [{ name: 'Hiro' }],
+    creator: 'Hiro',
+    publisher: 'n0va.one',
   },
-  authors: [{ name: 'luxxified.' }],
-  creator: 'luxxified.',
-  publisher: 'luxxified.',
+  '/battleye': {
+    title: 'n0va.one - BattlEye',
+    description: 'Owner @ Omni Services & Co-Owner @ n0va',
+    openGraph: {
+      title: 'n0va.one - BattlEye',
+      description: 'Owner @ Omni Services & Co-Owner @ n0va',
+      url: 'https://n0va.one/battleye',
+      siteName: 'n0va.one',
+      images: [
+        {
+          url: 'https://n0va.one/profile-picture1.png',
+          width: 800,
+          height: 800,
+          alt: 'BattlEye Preview',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'n0va.one - BattlEye',
+      description: 'Owner @ Omni Services & Co-Owner @ n0va',
+      creator: '@battleye',
+      images: ['https://n0va.one/profile-picture1.png'],
+    },
+    authors: [{ name: 'BattlEye' }],
+    creator: 'BattlEye',
+    publisher: 'n0va.one',
+  },
 }
 
-const foreverhiro = {
-  title: 'n0va.one - User | Hiro.',
-  description: 'Hiro, Student Athlete, Class of 29',
-  openGraph: {
-    title: 'n0va.one - User | Hiro.',
-    description: 'Hiro, Student Athlete, Class of 29',
-    url: 'https://n0va.one/foreverhiro',
-    siteName: 'n0va.one',
-    images: [
-      {
-        url: 'https://n0va.one/profile-picture.jpg',
-        width: 1000,
-        height: 1000,
-        alt: 'PFP',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'n0va.one - User | Hiro.',
-    description: 'Hiro, Student Athlete, Class of 29',
-    creator: '@luxxified',
-    images: ['https://n0va.one/profile-picture.jpg'],
-  },
-  authors: [{ name: 'luxxified.' }],
-  creator: 'luxxified.',
-  publisher: 'luxxified.',
-}
 export default function MetadataProvider({ children }: { children: ReactNode }) {
   const pathname = usePathname()
-
-  const metadata = pathname === '/1' ? specificMetadata : defaultMetadata
-  const metadata = pathname === '/foreverhiro' ? foreverhiro : defaultMetadata
+  const metadata = specificMetadata[pathname] || defaultMetadata
 
   return (
     <>
